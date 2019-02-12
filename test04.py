8d8d62c5d2f83b494d06c8ca7362fcbb913f8f15
横田しんじ <shinji@yokotashinjinoMacBook-ea.local>
2019-02-12 23:43:28 +0900

    リスト内包表記で記述

--- test04.py
+++ test04.py
@@ -1,7 +1,10 @@
-import re
-str = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nation Might Also Sign Peace Security Clause. Arther King Can.'
-w = str.split(' ')
-li = [re.match('[a-zA-z]',x).group() for x in w]
-#m = re.match('[a-zA-Z]',str).group()
+string = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nation Might Also Sign Peace Security Clause. Arther King Can.'
+w = string.split(' ')
 print(w)
-print(li)
+dic = {}
+for i, x in enumerate(w):
+    if i+1 not in (1,5,6,7,8,9,15,16,19):
+        dic[x[0:2]] = i+1
+    else:
+        dic[x[0:1]] = i+1
+print(dic)
