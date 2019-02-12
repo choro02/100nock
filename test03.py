5d71359ccfdcfd982eb2fbbdd856cc1a3d13fa9d
横田しんじ <shinji@yokotashinjinoMacBook-ea.local>
2019-02-12 22:56:28 +0900

    修正

--- test03.py
+++ test03.py
@@ -1,7 +1,6 @@
 import re
 str = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
 w = str.split(' ')
-li = [re.match('[a-zA-z]',x).group() for x in w]
-#m = re.match('[a-zA-Z]',str).group()
+li = [len(x) for x in w]
 print(w)
 print(li)
