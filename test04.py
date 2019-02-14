45598aded4619b721422e5b5cf5e94b46270d2e1
横田しんじ <shinji@yokotashinjinoMacBook-ea.local>
2019-02-14 12:31:22 +0900

    内包表記に修正

--- test04.py
+++ test04.py
@@ -1,10 +1,13 @@
 string = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nation Might Also Sign Peace Security Clause. Arther King Can.'
 w = string.split(' ')
 print(w)
-dic = {}
-for i, x in enumerate(w):
-    if i+1 not in (1,5,6,7,8,9,15,16,19):
-        dic[x[0:2]] = i+1
-    else:
-        dic[x[0:1]] = i+1
+t = (1,5,6,7,8,9,15,16,19)
+
+dic = {
+    (x[0:2] if i+1 not in t else x[0:1])
+    :i+1
+    for i, x
+    in enumerate(w)
+}
+
 print(dic)
