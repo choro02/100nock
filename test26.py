bb5fca60ac6128ce5d22abc55b3ca41867956473
横田しんじ <shinji@yokotashinjinoMacBook-ea.local>
2019-02-16 19:42:01 +0900

    ３章後半

---
+++ test26.py
@@ -0,0 +1,53 @@
+import gzip
+import json
+import re
+
+file = 'jawiki-country.json.gz'
+
+def article_UK():
+
+    with gzip.open(file, 'rt') as data:
+        for line in data:
+            data_json = json.loads(line)
+            if data_json['title'] == 'イギリス':
+                return (data_json['text'])
+                
+def remove_markup(target):
+    reg = re.compile(r'''
+        \'{2,5}
+        ''', re.MULTILINE + re.VERBOSE)
+    
+    return reg.sub('', target)
+
+reg1 = re.compile(r'''
+    ^\{\{
+    基礎情報.*?$(.*?)
+    \}\}$
+    ''', re.MULTILINE + re.VERBOSE + re.DOTALL)
+
+contents = reg1.findall(article_UK())
+
+reg2 = re.compile(r'''
+    ^\|
+    (.*?)
+    \s*
+    =
+    \s*
+    (.*?)
+    (?:
+        (?=\n\|)
+        | (?=\n$)
+    )
+    ''', re.MULTILINE + re.VERBOSE + re.DOTALL)
+
+fields = reg2.findall(contents[0])
+
+result = {}
+keys_test = []
+for field in fields:
+    result[field[0]] = remove_markup(field[1])
+    keys_test.append(field[0])
+
+for item in sorted(result.items(),
+        key=lambda field: keys_test.index(field[0])):
+    print(item)
---
+++ test29.py
@@ -0,0 +1,119 @@
+import gzip
+import json
+import re
+import urllib.parse, urllib.request
+
+file = 'jawiki-country.json.gz'
+
+def article_UK():
+
+    with gzip.open(file, 'rt') as data:
+        for line in data:
+            data_json = json.loads(line)
+            if data_json['title'] == 'イギリス':
+                return (data_json['text'])
+                
+def remove_markup(target):
+    reg = re.compile(r'''
+        (\'{2,5})
+        (.*?)
+        (\1)
+        ''', re.MULTILINE + re.VERBOSE)
+
+    target = reg.sub(r'\2', target)
+    
+    
+    reg = re.compile(r'''
+        \[\[
+        (?:
+            [^|]*?
+            \|
+        )*?
+        ([^|]*?)
+        \]\]
+        ''', re.MULTILINE + re.VERBOSE)
+
+    target = reg.sub(r'\1', target)
+    
+    reg = re.compile(r'''
+        \{\{lang
+        (?:
+            [^|]*?
+            \|
+        )*?
+        ([^|]*?)
+        \}\}
+        ''', re.MULTILINE + re.VERBOSE)
+
+    target = reg.sub(r'\1', target)
+
+    reg = re.compile(r'''
+        \[http:\/\/
+        (?:
+            [^\s]*?
+            \s
+        )?
+        ([^]]*?)
+        \]
+        ''', re.MULTILINE + re.VERBOSE)
+
+    target = reg.sub(r'\1', target)
+
+    reg = re.compile(r'''
+        <
+        \/?
+        [br|ref]
+        [^>]*?
+        >
+        ''', re.MULTILINE + re.VERBOSE)
+
+    target = reg.sub('', target)
+
+    return target
+
+
+
+reg1 = re.compile(r'''
+    ^\{\{
+    基礎情報.*?$(.*?)
+    \}\}$
+    ''', re.MULTILINE + re.VERBOSE + re.DOTALL)
+
+contents = reg1.findall(article_UK())
+
+reg2 = re.compile(r'''
+    ^\|
+    (.*?)
+    \s*
+    =
+    \s*
+    (.*?)
+    (?:
+        (?=\n\|)
+        | (?=\n$)
+    )
+    ''', re.MULTILINE + re.VERBOSE + re.DOTALL)
+
+fields = reg2.findall(contents[0])
+
+result = {}
+for field in fields:
+    result[field[0]] = remove_markup(field[1])
+
+fname_flag = result['国旗画像']
+
+url = 'https://www.mediawiki.org/w/api.php?' \
+    + 'action=query' \
+    + '&titles=File:' + urllib.parse.quote(fname_flag) \
+    + '&format=json' \
+    + '&prop=imageinfo' \
+    + '&iiprop=url'
+
+request = urllib.request.Request(url,
+    headers={'User-Agent': 'NLP100_Python(@segavvy)'})
+connection = urllib.request.urlopen(request)
+
+data = json.loads(connection.read().decode())
+
+url = data['query']['pages'].popitem()[1]['imageinfo'][0]['url']
+print(url)
