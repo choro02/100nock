6e6ca41dd2987b677106960feabc81f0242d5d05
横田しんじ <shinji@yokotashinjinoMacBook-ea.local>
2019-02-19 17:50:24 +0900

    削除

--- twitter_practice/config.py
+++
@@ -1,9 +0,0 @@
-#Twitter APIを使用するためのConsumerキー、アクセストークン設定
-# '<TwitterAPI登録申請して取得した API key>'
-Consumer_key = 'B80vMjUjFW7MyghF5J3EozVtG'
-#'<TwitterAPI登録申請して取得したConsumer_secret>'
-Consumer_secret = 'IrMQmfRoZPc5bi20jKXOp637WQ0gpapQY4jPD4HdpDXXqobr4b'
-#'<TwitterAPI登録申請して取得したAccess_token>'
-Access_token = '1097709628798906369-z19mVIVHeyqmCJ4j7SLhPqZPhWLo13'
-#'<TwitterAPI登録申請して取得したAccess_secret>'
-Access_secret = '9iHIWWT1xZUEFNVj09Mpc7ZETVcI02SQu7sMlkLTZEVVK'
